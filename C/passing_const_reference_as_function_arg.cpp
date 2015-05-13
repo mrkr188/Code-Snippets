b8a7136e869de21dc97ab75ae98952c040bb97ee
mrkr188 <medarajeevkanthreddy@gmail.com>
2015-05-13 15:40:17 +0530

    Create passing_const_reference_as_function_arg.cpp

---
+++ C/passing_const_reference_as_function_arg.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+    using namespace std;
+
+    //int fun(int &n) //compiler will give error if we pass const reference.
+                      //Cant pass const reference unless argument is const int &n..
+    int fun(const int  &n){
+        return n;
+    }
+
+
+int main()
+{
+
+    int const i = 10;
+
+    cout<<fun(i);  //passing const int as reference.
+
+    return 0;
+}
