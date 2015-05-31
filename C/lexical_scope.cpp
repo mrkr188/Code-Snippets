dca630729f29cdb613dbd648f6cdffe51eb7e836
mrkr188 <medarajeevkanthreddy@gmail.com>
2015-05-31 21:45:21 +0530

    Update lexical_scope.cpp

--- C/lexical_scope.cpp
+++ C/lexical_scope.cpp
@@ -13,9 +13,6 @@ int g(){
     int x = 10;
     return f();  //x in f() is always bound to the variable when its declared. Lexical scoping
                  //method gets its definition in the environment it is declared, not in the one is called.
-
-
-
 }
 
 
