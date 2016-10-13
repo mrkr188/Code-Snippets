1e0822c9387fce9a122284b01c4316cb63b643d7
mrkr188 <medarajeevkanthreddy@gmail.com>
2016-10-13 02:16:17 -0400

    Create basics_array1.c

---
+++ C/basics_array1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int fun(int ptr[])
+{
+int x = 20;
+
+// size of a pointer is printed
+printf("sizeof(ptr) = %d\n", sizeof(ptr));
+printf("*ptr = %d \n", *ptr);
+
+// ptr now points to array
+ptr[1] = 21;
+printf("%d", arr[1]);
+
+
+// Assigning any address to an array variable is not allowed.
+// But here this allowed because ptr is a pointer once its passed into function, not array
+// ptr does not point to array now
+ptr = &x;
+
+printf("*ptr = %d \n", *ptr);
+
+return 0;
+}
+int main()
+{
+int arr[] = {10, 20, 30, 40, 50, 60};
+fun(arr);
+printf("%d \n", arr[0]);
+printf("%d \n", arr[1]);
+
+return 0;
+}
