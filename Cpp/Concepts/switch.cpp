bb0b64e6802f8b9954c455ba462e1ecf6dcbe231
mrkr188 <medarajeevkanthreddy@gmail.com>
2017-06-21 01:56:29 -0400

    Update switch.cpp

--- Cpp/Concepts/switch.cpp
+++ Cpp/Concepts/switch.cpp
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
+#include <stdio.h>
+
 // printf("Outside "); doesnt get executed. Its almost like a comment. 
 // in switch, control directly moves to cases
 
 // other things to mind in switch statements are
 // 1) cases can only have int, char, enum values
 // 2) cases cant have same values
+// 3) case values must be constant. for example if we define int arr[] = {1,2,3}; and 
+//    use arr[0], arr[1], arr[2] as case values it gives compile time error: case label 
+//    does not reduce to an integer constant
 
 int main()
 {
