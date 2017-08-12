1cf18d67d63a26d740cbf6db43b5178067df2825
mrkr188 <medarajeevkanthreddy@gmail.com>
2017-08-12 12:13:51 -0400

    Create array_passing_2d_array.cpp

---
+++ Cpp/Concepts/array_passing_2d_array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int getVal(int (*A)[3], int i, int j){
+    return A[i][j];
+}
+
+int main(){
+    
+    int A[][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    
+    cout << getVal(A,1,2) << endl;
+}
+
+
